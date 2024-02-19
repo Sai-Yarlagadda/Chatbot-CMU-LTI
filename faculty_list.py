43bfcf796e6615f46d4277f18a1f84f23f8f6040
Sai Sravan Yarlagadda <144404355+Sai-Yarlagadda@users.noreply.github.com>
2024-02-19 16:44:36 -0500

    Update faculty_list.py

--- faculty_list.py
+++ faculty_list.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
 
 def get_workers_info(url):
     page = requests.get(url)
@@ -11,12 +12,34 @@ def get_workers_info(url):
 
     return workers_names
 
+def schedule_of_classes(url):
+    response = requests.get(url)
 
+    if response.status_code == 200:
+        html_content= response.text
+        soup = BeautifulSoup(html_content, 'html.parser')
+        
+        rows = soup.find_all('tr')
+
+        with open('Schedule_of_classes.csv', 'w', newline='', encoding='utf-8') as csvfile:
+            csv_writer = csv.writer(csvfile)
+
+            for row in rows:
+                columns = row.find_all('td')
+                row_data = [column.get_text(strip=True) for column in columns]
+                csv_writer.writerow(row_data)
+                
+    else:
+        print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
 
 #all urls
 url_faculty = 'https://lti.cs.cmu.edu/directory/all/154/1'
 url_affiliated_faculty = 'https://lti.cs.cmu.edu/directory/all/154/2728'
 url_adjunct_faculty = 'https://lti.cs.cmu.edu/directory/all/154/200'
+url_schedule_of_classes = 'https://enr-apps.as.cmu.edu/assets/SOC/sched_layout_spring.htm'
 
+
+#This will make a csv file of with the timetable of the classes
+schedule_of_classes(url_schedule_of_classes)
 
 
