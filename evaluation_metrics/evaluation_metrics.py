2d6c1d23afded7fa4ac1ad6b7a66428defeebe06
Sai Sravan Yarlagadda <144404355+Sai-Yarlagadda@users.noreply.github.com>
2024-03-11 23:42:25 -0400

    Update evaluation_metrics.py

--- evaluation_metrics/evaluation_metrics.py
+++ evaluation_metrics/evaluation_metrics.py
@@ -1,148 +1,96 @@
-from langchain.evaluation import ExactMatchStringEvaluator
+"""
+reference: https://storageclwsprod1.blob.core.windows.net/bundles/0x6b567e1cf2e041ec80d7098f031c5c9e/contents.gz?se=2024-03-12T03%3A01%3A59Z&sp=rt&sv=2019-12-12&sr=b&rscd=inline%3B%20filename%3D%22evaluate-v2.0.py%22&rsce=gzip&rsct=text/x-python&sig=L68GoD7aDWB84B206a9jxqrn3wMvEshepkr2IEWXsQg%3D
+"""
+import collections
+import re
 import string
 
-def exact_match(correct_answers_path, generated_answers_path):
-    """This is a function that calculates the exact match of 
-    generated answer and correct answers"""
+def normalize_answer(s):
+  """Lower text and remove punctuation, articles and extra whitespace."""
+  def remove_articles(text):
+    regex = re.compile(r'\b(a|an|the)\b', re.UNICODE)
+    return re.sub(regex, ' ', text)
+  def white_space_fix(text):
+    return ' '.join(text.split())
+  def remove_punc(text):
+    exclude = set(string.punctuation)
+    return ''.join(ch for ch in text if ch not in exclude)
+  def lower(text):
+    return text.lower()
+  return white_space_fix(remove_articles(remove_punc(lower(s))))
 
-    evaluator = ExactMatchStringEvaluator(ignore_case=True, ignore_punctuation=True)
-    with open(generated_answers_path, 'r') as generated_answers:
-        gen_answers = generated_answers.read().split('==================\n')
-    
-    with open(correct_answers_path, 'r') as correct_answers:
-        corr_answers = correct_answers.readlines()
+def get_tokens(s):
+  if not s: return []
+  return normalize_answer(s).split()
 
-    exact_match_scores = 0
+def compute_exact(a_gold, a_pred):
+  return int(normalize_answer(a_gold) == normalize_answer(a_pred))
 
-    for i in range(len(gen_answers)):
-        scores = evaluator.evaluate_strings(
-            prediction=gen_answers[i],
-            reference=corr_answers[i],
-        )
-        exact_match_scores += int(scores['score'])
+def compute_f1(a_gold, a_pred):
+  gold_toks = get_tokens(a_gold)
+  pred_toks = get_tokens(a_pred)
+  common = collections.Counter(gold_toks) & collections.Counter(pred_toks)
 
-    
-    return exact_match_scores
+  if not common:
+    f1 = 0
+    precision = 0
+    recall = 0
 
-def remove_punctuation(text):
-    """
-    Remove punctuations from the input text
-    """
-    return text.translate(str.maketrans("", "", string.punctuation))
+  else:
+    num_same = sum(common.values())
+    if len(gold_toks) == 0 or len(pred_toks) == 0:
+        # If either is no-answer, then F1 is 1 if they agree, 0 otherwise
+        return int(gold_toks == pred_toks)
+    if num_same == 0:
+        return 0
+    precision = 1.0 * num_same / len(pred_toks)
+    recall = 1.0 * num_same / len(gold_toks)
+    f1 = (2 * precision * recall) / (precision + recall)
 
-def precision(reference, test):
-    """
-    Calculate the precision based on the generated answer and the correct answer
-    """
-    if not hasattr(reference, "intersection") or not hasattr(test, "intersection"):
-        raise TypeError("reference and test should be sets")
-   
-    #remove punctuation 
-    reference = set(remove_punctuation(word) for word in reference)
-    test = set(remove_punctuation(word) for word in test)
+  return f1, precision, recall
 
-    #remove case sensitivity
-    reference = set(remove_punctuation(word.lower()) for word in reference)
-    test = set(remove_punctuation(word.lower()) for word in test)
-
-    if len(test) == 0:
-        return None
-    else:
-        return len(reference.intersection(test)) / len(test)
-
-def recall(reference, test):
-    """
-    Calculate the recall based on generated answer and the correct answer
-    """
-    if not hasattr(reference, "intersection") or not hasattr(test, "intersection"):
-        raise TypeError("reference and test should be sets")
-
-
-    #remove punctuation 
-    reference = set(remove_punctuation(word) for word in reference)
-    test = set(remove_punctuation(word) for word in test)
-
-    #remove case sensitivity
-    reference = set(remove_punctuation(word.lower()) for word in reference)
-    test = set(remove_punctuation(word.lower()) for word in test)
-
-    if len(reference) == 0:
-        return None
-    else:
-        return len(reference.intersection(test)) / len(reference)
-
-
-def f1(reference_path, test_path):
-    """
-    Calculating the F1 score based on precision and recall
-    F1 = (2 * precision_val * recall_val) / (precision_val + recall_val)
-    """
-
-    with open(test_path, 'r') as file:
-        generated_answers = file.read().replace('\n', '').split('==================')
-    
-    with open(reference_path, 'r') as file:
-        correct_answers = file.readlines()
-
-    final_f1 = 0
-    final_precision = 0
-    final_recall = 0
-
-    with open('comp-genanswers-corranswers.txt', 'w') as file:
-        for i in range(len(generated_answers)):
-            if not generated_answers[i].strip():
-                continue
-
-            reference_answers = correct_answers[i].strip().split(';')
-            best_f1_score = 0
-            best_precision_val = 0
-            best_recall_val = 0
-
-            for reference_answer in reference_answers:
-                gen_answer_set = set(generated_answers[i])
-                correct_answer_set = set(reference_answer)
-
-                precision_val = precision(correct_answer_set, gen_answer_set)
-                recall_val = recall(correct_answer_set, gen_answer_set)
-
-                try:
-                    f1score = (2 * float(precision_val) * float(recall_val)) / (float(precision_val) + float(recall_val))
-                
-                except ZeroDivisionError:
-                    f1score = 0
-
-                if f1score > best_f1_score:
-                    best_f1_score = f1score
-                    best_precision_val = precision_val
-                    best_recall_val = recall_val
-
-            file.write(f'The best F1 score is {best_f1_score}\n')
-            file.write(f'Precision: {best_precision_val}, Recall: {best_recall_val}\n')
-            file.write(f'generated_Answer is : {generated_answers[i]}\ngiven answer is: {correct_answers[i]}\n===========\n')
-            final_f1 += best_f1_score
-            final_precision += best_precision_val
-            final_recall += best_recall_val
-    
-    f1_score_complete_data = final_f1 / len(correct_answers)
-    precision_complete_data = final_precision / len(correct_answers)
-    recall_complete_data = final_recall / len(correct_answers)
-    return f1_score_complete_data, precision_complete_data, recall_complete_data
 
 if __name__ == '__main__':
-    generated_answers_path = 'rag_with_reranker_answers.txt'
-    correct_answers_path = 'answers.txt'
+  generated_answers_path = 'rag_with_reranker_with_multiquery_answers.txt'
+  correct_answers_path = 'answers.txt'
 
-    f1_score = f1(correct_answers_path, generated_answers_path)
-    print(f1_score)
+  tot_f1 = tot_precision = tot_recall = exact_match =0
+  with open(generated_answers_path, 'r') as f:
+    pred_ans = f.read().replace('\n', '').split('==================')
 
-if __name__ == '__main__':
-    generated_answers_path = 'rag_with_reranker_answers.txt'
-    correct_answers_path = 'answers.txt'
+  with open(correct_answers_path, 'r') as f:
+    corr_ans = f.readlines()
 
-    exact_match_score = exact_match('answers.txt', 'rag_with_reranker_answers.txt')
-    
-    '''f1_score = f1(correct_answers_path, generated_answers_path)'''
+  with open('eval_for_reranker_with_multiquery_answers.txt', 'w') as out_file:
+    for i in range(len(corr_ans)):
 
-    f1_score = f1('answers.txt', 'rag_with_reranker_answers.txt')
-    print(exact_match_score)
-    print(f1_score)
+        reference_answers = corr_ans[i].strip().split(';')
+        best_f1 = 0
+        best_precision_val = 0
+        best_recall = 0
+        for reference_answer in reference_answers:
+          f1, prec, rec = compute_f1(reference_answer, str(pred_ans[i]))
+          if f1 >= best_f1:
+            best_f1 = f1
+            best_precision_val = prec
+            best_recall = rec
+        f1 = best_f1
+        prec = best_precision_val
+        rec = best_recall
+
+        '''f1, prec, rec = compute_f1(str(corr_ans[i]), str(pred_ans[i]))'''
+        tot_f1 += f1
+        tot_precision += prec
+        tot_recall += rec
+        ex_mtch = compute_exact(corr_ans[i], pred_ans[i])
+        exact_match+=ex_mtch
+        out_file.write(f'Predicted answer: {pred_ans[i]}\nCorrect answer: {corr_ans[i]}\nf1: {f1}\nPrecision: {prec}\nRecall: {rec}\nExact Match: {ex_mtch}\n===========\n')
+
+  
+  f1 = tot_f1 / len(pred_ans)
+  precision = tot_precision / len(pred_ans)
+  recall = tot_recall / len(pred_ans)
+  print(f'Average F1: {f1}')
+  print(f'Average precision: {precision}')
+  print(f'Average recall: {recall}')
+  print(f'Exact Match: {exact_match}')
